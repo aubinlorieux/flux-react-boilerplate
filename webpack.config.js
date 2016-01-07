6faef15d2fbac4856ebd9c8e16d2ea4d143cad70
Aubin LORIEUX <aubin.lorieux@winddle.com>
2016-01-07 11:13:48 +0100

    add less, bootstrap to the pipeline

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 /* eslint-disable no-var */
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var nodeModulePath = path.resolve(__dirname, 'node_modules');
+var buildPath = path.resolve(__dirname, 'public', 'build');
+var srcPath = path.resolve(__dirname, 'src');
+var mainPath = path.resolve(__dirname, 'src', 'app.js');
+var stylesheetPath = path.resolve(__dirname, 'src', 'app.less');
 
 module.exports = {
 	entry: [
-		'webpack-dev-server/client?http://localhost:3000',
-		'webpack/hot/only-dev-server',
-		'./scripts/index.js'
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    mainPath,
+    stylesheetPath
 	],
 	devtool: 'eval-source-map',
 	output: {
-		path: __dirname,
+		path: buildPath,
 		filename: 'bundle.js',
-		publicPath: '/public/'
+		publicPath: '/build/'
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin()
+		new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('style.css', {
+        allChunks: true
+    })
 	],
 	resolve: {
 		extensions: ['', '.js']
@@ -25,7 +36,31 @@ module.exports = {
 		loaders: [{
 			test: /\.jsx?$/,
 			loaders: ['react-hot', 'babel'],
-			include: path.join(__dirname, 'scripts')
-		}]
+			include: srcPath
+		},
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
+    },
+		{
+			test: /\.less$/,
+			loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+		},
+		{
+    	test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=application/font-woff'
+    },
+    {
+    	test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=application/octet-stream'
+    },
+    {
+    	test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'file'
+    },
+    {
+    	test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=image/svg+xml'
+    }]
 	}
 };
--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,12 +1,22 @@
 /* eslint-disable no-var */
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var nodeModulePath = path.resolve(__dirname, 'node_modules');
+var distPath = path.resolve(__dirname, 'dist');
+var srcPath = path.resolve(__dirname, 'src');
+var mainPath = path.resolve(__dirname, 'src', 'app.js');
+var stylesheetPath = path.resolve(__dirname, 'src', 'app.less');
 
 module.exports = {
 	devtool: 'source-map',
-	entry: './scripts/index',
+	entry: [
+		mainPath,
+    stylesheetPath
+	],
 	output: {
-		path: path.join(__dirname, 'dist'),
+		path: distPath,
 		filename: 'bundle.js',
 		publicPath: '/'
 	},
@@ -15,6 +25,9 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('style.css', {
+        allChunks: true
+    }),
 		new webpack.DefinePlugin({
 			'process.env': {
 				'NODE_ENV': JSON.stringify('production')
@@ -30,7 +43,31 @@ module.exports = {
 		loaders: [{
 			test: /\.jsx?$/,
 			loaders: ['babel'],
-			include: path.join(__dirname, 'scripts')
-		}]
+			include: srcPath
+		},
+    {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
+    },
+    {
+      test: /\.less$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+    },
+		{
+    	test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=application/font-woff'
+    },
+    {
+    	test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=application/octet-stream'
+    },
+    {
+    	test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'file'
+    },
+    {
+    	test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+    	loader: 'url?limit=10000&mimetype=image/svg+xml'
+    }]
 	}
 };
